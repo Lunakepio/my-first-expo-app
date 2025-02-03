2afb16dcea279f1577258dd6f7e8edf4e05e3f08
Lunakepio <moulinneufalex@gmail.com>
2025-02-03 10:40:09 +0100

    push

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { useStore } from './store/store';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useStore } from "./store/store";
 
 export default function App() {
-  const { count, increment } = useStore();
+  const { count, increment, decrement } = useStore();
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={increment}><Text style={styles.text}>{count}</Text></TouchableOpacity>
+      <Text style={styles.text}>{count}</Text>
+      <View style={styles.row}>
+        <TouchableOpacity onPress={decrement} style={[styles.modifier]}>
+          <Text style={{ fontWeight: 600, fontSize: 40, color: "white" }}>-</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={increment} style={[styles.modifier, {backgroundColor: "red"}]}>
+          <Text style={{ fontWeight: 600, fontSize: 40, color: "white" }}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -14,13 +22,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 32,
   },
   text: {
     fontSize: 98,
     fontWeight: 600,
-    
-  }
+  },
+  row: {
+    flexDirection: "row",
+    gap: 32,
+  },
+  modifier: {
+    backgroundColor: "#093EFC",
+    width: 120,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8,
+  },
 });
--- store/store.js
+++ store/store.js
@@ -3,4 +3,5 @@ import { create } from "zustand";
 export const useStore = create((set) => ({
   count: 0,
   increment: () => set((state) => ({count: state.count + 1 })),
+  decrement: () => set((state) => ({count: state.count - 1}))
 }));
